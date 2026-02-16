bfa8bebc38e309b7bdae03a8f6fdde5de4742d94
MANISH KUMAR AGRAHARI <mka9146866@gmail.com>
2026-02-17 02:01:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Factorial/factorial.java
@@ -0,0 +1,24 @@
+import java.util.Scanner ;
+class Solution {
+    // Function to calculate factorial of a number.
+public static int  factorial(int n) {
+        if(n==0 || n==1){
+            
+            return 1 ;
+        }
+        else {
+            
+            return factorial(n-1)*n;
+        }
+    }
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        int result = factorial(n);
+        
+        System.out.println(result);
+    }
+}
