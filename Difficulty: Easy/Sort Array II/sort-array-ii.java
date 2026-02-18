cc19c6b48ed0f942e93a1146c62ee4aaeb80a98d
MANISH KUMAR AGRAHARI <mka9146866@gmail.com>
2026-02-19 00:02:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sort Array II/sort-array-ii.java
@@ -0,0 +1,10 @@
+class Solution {
+    void sortArr(int[] arr) {
+        // code here
+        // for(int i= 0; i<arr.length; i++){
+        //     if(arr[i]>arr[i+1]){
+                Arrays.sort(arr);
+            }
+        
+    
+}
